a93f323556d3b9f2afa36b3dd4b079a0fa43fcdc
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2023-04-08 13:18:01 +0200

    Init

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 /* SSDV - Slow Scan Digital Video                                        */
 /*=======================================================================*/
 /* Copyright 2011-2016 Philip Heron <phil@sanslogic.co.uk>               */
